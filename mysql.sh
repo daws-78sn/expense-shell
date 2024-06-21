316f67aa8e987abb0043c80bb62411e5d4175731
joindevops <skreddy2244@gmail.com>
2024-06-22 00:21:12 +0530

    expense

--- mysql.sh
+++ mysql.sh
@@ -36,8 +36,18 @@ VALIDATE $? "Enabling MySQL Server is.."
 systemctl start mysqld &>>$LOGFILE
 VALIDATE $? "Starting MySQL Server is.."
 
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
-VALIDATE $? "Setting up root password"
+#commenting below command
+#mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$LOGFILE
+#VALIDATE $? "Setting up root password"
 
 #to overcome Idempotency use following command
 #mysql -uroot -pExpenseApp@1 -e "CREATE DATABASE expenseapp;
+
+mysql -h db.daws78s.online -uroot -pExpenseApp@1 &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+    mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE
+    VALIDATE $? "MySQL Root password setup"
+else
+    echo "mySQL Root password is already setup...$Y SKIPPING $N"
+fi
